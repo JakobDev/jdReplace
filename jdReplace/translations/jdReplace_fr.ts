ac5bd8d0c70364142addab847b923977eb8726b5
Francewhoa <francois+codeberg.org@ubertus.ca>
2023-12-04 15:46:32 +0000

    Added translation using Weblate (French)

---
+++ jdReplace/translations/jdReplace_fr.ts
@@ -0,0 +1,136 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>AboutWindow</name>
+    <message>
+        <location filename="../__init__.py" line="28"/>
+        <source>With this program you can search and replace a text in all files of a folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="30"/>
+        <source>This program is licensed under GNU GPL 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="31"/>
+        <source>View Source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="32"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="69"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ReplaceThread</name>
+    <message>
+        <location filename="../__init__.py" line="95"/>
+        <source>Searching {{path}}...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="137"/>
+        <source>Could not replace text in {{path}}. Maybe it&apos;s a binary file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StartWindow</name>
+    <message>
+        <location filename="../__init__.py" line="151"/>
+        <source>Directory:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="217"/>
+        <location filename="../__init__.py" line="153"/>
+        <source>Browse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="154"/>
+        <source>Search for:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="156"/>
+        <source>Replace with:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="158"/>
+        <source>Search Subdirectories</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="159"/>
+        <source>Skip Hidden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="160"/>
+        <source>Follow Symlinks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="162"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="237"/>
+        <location filename="../__init__.py" line="163"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="241"/>
+        <location filename="../__init__.py" line="240"/>
+        <source>Finished</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="241"/>
+        <source>The text has been successfully replaced in all files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="243"/>
+        <source>Canceled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="254"/>
+        <source>Not a directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="254"/>
+        <source>&apos;{{path}}&apos; is not a directory!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="259"/>
+        <source>No search text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="259"/>
+        <source>Please enter a text to search for</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../__init__.py" line="263"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
