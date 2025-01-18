933e5f5f30e5fb520060d0ed2b084d422477b119
Priit Jõerüüt <jrtcdbrg@users.noreply.translate.codeberg.org>
2025-01-18 14:09:31 +0000

    Translated using Weblate (Estonian)
    Currently translated at 100.0% (24 of 24 strings)
    
    Translation: jdReplace/jdReplace
    Translate-URL: https://translate.codeberg.org/projects/jdReplace/jdReplace/et/

--- jdReplace/translations/jdReplace_et.ts
+++ jdReplace/translations/jdReplace_et.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../__init__.py" line="28"/>
         <source>With this program you can search and replace a text in all files of a folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Selle rakendusega saad otsida ja asendada teksti kõikides ühe kausta failides</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="30"/>
         <source>This program is licensed under GNU GPL 3</source>
-        <translation type="unfinished"></translation>
+        <translation>See rakendus on litsentseeritud GNU GPL versioon 3 alusel</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="31"/>
         <source>View Source</source>
-        <translation type="unfinished"></translation>
+        <translation>Vaata lähtekoodi</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="32"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Sulge</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="69"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Rakenduse teave</translation>
     </message>
 </context>
 <context>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../__init__.py" line="95"/>
         <source>Searching {{path}}...</source>
-        <translation type="unfinished"></translation>
+        <translation>Otsime asukohas {{path}}...</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="137"/>
         <source>Could not replace text in {{path}}. Maybe it&apos;s a binary file.</source>
-        <translation type="unfinished"></translation>
+        <translation>Teksti asendamine asukohas {{path}} ei õnnestunud. Võib-olla on tegemist kompileeritud failiga.</translation>
     </message>
 </context>
 <context>
@@ -47,90 +47,90 @@
     <message>
         <location filename="../__init__.py" line="151"/>
         <source>Directory:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kaust:</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="217"/>
         <location filename="../__init__.py" line="153"/>
         <source>Browse</source>
-        <translation type="unfinished"></translation>
+        <translation>Sirvi</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="154"/>
         <source>Search for:</source>
-        <translation type="unfinished"></translation>
+        <translation>Otsi selliseid sõnu:</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="156"/>
         <source>Replace with:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ning asenda nad sellega:</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="158"/>
         <source>Search Subdirectories</source>
-        <translation type="unfinished"></translation>
+        <translation>Otsi ka alamkaustadest</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="159"/>
         <source>Skip Hidden</source>
-        <translation type="unfinished"></translation>
+        <translation>Jäta peidetud failid vahele</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="160"/>
         <source>Follow Symlinks</source>
-        <translation type="unfinished"></translation>
+        <translation>Järgi sümbolviiteid</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="162"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Rakenduse teave</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="237"/>
         <location filename="../__init__.py" line="163"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>Sobib</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="241"/>
         <location filename="../__init__.py" line="240"/>
         <source>Finished</source>
-        <translation type="unfinished"></translation>
+        <translation>Valmis</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="241"/>
         <source>The text has been successfully replaced in all files</source>
-        <translation type="unfinished"></translation>
+        <translation>Teksti asendamine õnnestus kõikides failides</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="243"/>
         <source>Canceled</source>
-        <translation type="unfinished"></translation>
+        <translation>Katkestatud</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="254"/>
         <source>Not a directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Tegemist pole kaustaga</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="254"/>
         <source>&apos;{{path}}&apos; is not a directory!</source>
-        <translation type="unfinished"></translation>
+        <translation>„{{path}}“ pole kaust!</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="259"/>
         <source>No search text</source>
-        <translation type="unfinished"></translation>
+        <translation>Otsitav tekst on sisestamata</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="259"/>
         <source>Please enter a text to search for</source>
-        <translation type="unfinished"></translation>
+        <translation>Palun sisesta tekst, mida soovid otsida</translation>
     </message>
     <message>
         <location filename="../__init__.py" line="263"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Katkesta</translation>
     </message>
 </context>
 </TS>
